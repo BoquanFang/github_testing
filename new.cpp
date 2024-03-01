40b923cc70771387281c5c4aed1d3b4693ef3029
BoquanFang <david.fang200251@gmail.com>
2024-02-29 23:36:27 -0800

    Merge two commit

--- new.cpp
+++ new.cpp
@@ -10,5 +10,6 @@ int main() {
     print_function();
     std::cout << "Hello World!" << std::endl;
     std::cout << "Hello World! Another Time!" << std::endl;
+    std::cout << "Hello World! MERGE TWO!" << std::endl;
     return 0;
 }
